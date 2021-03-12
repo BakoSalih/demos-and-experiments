66349b5422f84621448691ba077a3dd0f2bbb9e3
Bako <bakosalih@gmail.com>
2021-03-12 11:06:29 +0100

    changed colors, changed how holes are handled, fixed some bugs

--- matching.js
+++ matching.js
@@ -1,1 +1,321 @@
-function Reduction(t){const n=t.length,o=n/2;let e=new Float64Array(n*n),r=new Int32Array(n*n);for(let o=0;o<n;o++)for(let l=0;l<n;l++)r[l*n+o]=0==t[o][l],e[l*n+o]=t[o][l]+(0==t[o][l]);const l=Math.ceil(t.length/2),c=new Float32Array(l*(l+1)*(2*l+1)/6);let s=-1;for(let t=0;t<=o-1;t++){const l=(n+1)*t;for(let f=o-t;f--;)for(let a=o-t;a--;){s++;const t=2*(f*n+a)+l,o=r[t],i=r[t+1],h=r[t+n],u=r[t+n+1],g=o+u,w=h+i;if(w>g){r[t]*=-1,r[t+1]=h-g,r[t+n]=i-g,r[t+n+1]*=-1;const o=e[t+1],l=1/e[t],f=1/e[t+n+1];e[t+1]=e[t+n]*l*f,e[t+n]=o*l*f,e[t]=l,e[t+n+1]=f,c[s]=1}else if(g==w){r[t]*=-1,r[t+1]*=-1,r[t+n]*=-1,r[t+n+1]*=-1;const o=e[t],l=e[t+1],f=e[t+n],a=e[t+n+1];let i=o*a+f*l;e[t]=a/i,e[t+1]=f/i,e[t+n]=l/i,e[t+n+1]=o/i,c[s]=o*a/i}else{r[t]=u-w,r[t+1]*=-1,r[t+n]*=-1,r[t+n+1]=o-w;const l=e[t],c=1/e[t+1],s=1/e[t+n];e[t]=e[t+n+1]*s*c,e[t+n+1]=l*s*c,e[t+1]=c,e[t+n]=s}}}return c}function Matching(t,n){const o=new Uint8Array((n+2)*(n+2)),e=n/2;let r=n+2;const l=t.length;for(let n=0;n<=e;n++){const c=(1+r)*(e-n+1),s=[];for(let t=0;t<=2*(n-1);t+=2)for(let e=0;e<=2*(n-1);e+=2){const n=t*r+e+c;switch(o[n]+o[n+1]+o[n+r]+o[n+r+1]){case 1:const l=o[n];o[n]=o[n+r+1],o[n+r+1]=l;const c=o[n+r];o[n+r]=o[n+1],o[n+1]=c;break;case 2:o[n]=0,o[n+1]=0,o[n+r]=0,o[n+r+1]=0,s.push(t,e);break;case 0:s.push(t,e)}}for(let e=0,f=s.length;e<f;e+=2){const f=s[e],a=s[e+1],i=f*r+a+c;if(!(o[i-1]||o[i-r]||o[i+2]||o[i+r-1])){const e=l-(a/2*n+f/2+1+(n-1)*n*(2*n-1)/6);Math.random()<t[e]?(o[i]=1,o[i+r+1]=1):(o[i+1]=1,o[i+r]=1)}}}const c=[];for(let t=n;t>=1;t--){const e=new Uint8Array(n);for(let l=1;l<=n;l++)e[l-1]=o[t*r+l];c.push(e)}return c}function Coloring(t,n){const o=new Float32Array((n+2)*(n+2)),e=n/2;let r=n+2,l=t.length;for(let n=0;n<=e;n++){const c=(1+r)*(e-n+1);for(let e=0;e<=2*(n-1);e+=2)for(let s=0;s<=2*(n-1);s+=2){const f=e*r+s+c,a=o[f],i=o[f+1],h=o[f+r],u=o[f+r+1],g=1-a-i-h-u,w=t[l-(s/2*n+e/2+1+(n-1)*n*(2*n-1)/6)];o[f]=u+g*w,o[f+1]=h+g*(1-w),o[f+r]=i+g*(1-w),o[f+r+1]=a+g*w}}const c=[];for(let t=n;t>=1;t--){const e=new Float64Array(n);for(let l=1;l<=n;l++)e[l-1]=o[t*r+l];c.push(e)}return c}export{Reduction,Matching,Coloring};
+function Reduction(W) {
+
+  const n = W.length;
+  const m = n/2;
+
+  let W_real = new Float64Array(n*n);
+  let W_im = new Int32Array(n*n);
+
+  for (let j=0; j<n; j++) {
+    for (let i=0; i<n; i++) {
+       W_im[i*n + j] = (W[j][i] == 0);
+       W_real[i*n + j] = W[j][i] + (W[j][i] == 0);
+        //  if (W[i][j] == 0) {
+        //  W_im[i*n + j] = 1;
+        //  W_real[i*n + j] = 1;
+        //  } else {
+        //  W_im[i*n + j] = 0;
+        //  W_real[i*n + j] = W[i][j];
+        // }
+     }
+  }
+
+  // let W_real1 = new Float64Array(m*m);
+  // let W_real2 = new Float64Array(m*m);
+  // let W_real3 = new Float64Array(m*m);
+  // let W_real4 = new Float64Array(m*m);
+  // let W_im1 = new Int32Array(m*m);
+  // let W_im2 = new Int32Array(m*m);
+  // let W_im3 = new Int32Array(m*m);
+  // let W_im4 = new Int32Array(m*m);
+  //
+  // for (let i=0; i<m; ++i) {
+  //    for (let j=0; j<m; ++j) {
+  //      W_im1[i*m + j] = (W[2*i][2*j] == 0);
+  //      W_im2[i*m + j] = (W[2*i+1][2*j] == 0);
+  //      W_im3[i*m + j] = (W[2*i][2*j+1] == 0);
+  //      W_im4[i*m + j] = (W[2*i+1][2*j+1] == 0);
+  //      W_real1[i*m + j] = W[2*i][2*j]     + (W[2*i][2*j]     == 0);
+  //      W_real2[i*m + j] = W[2*i+1][2*j]   + (W[2*i+1][2*j]   == 0);
+  //      W_real3[i*m + j] = W[2*i][2*j+1]   + (W[2*i][2*j+1]   == 0);
+  //      W_real4[i*m + j] = W[2*i+1][2*j+1] + (W[2*i+1][2*j+1] == 0);
+  //    }
+  // }
+
+  // console.log(W_im1);
+  // console.log(W_im2);
+  // console.log(W_im3);
+  // console.log(W_im4);
+  // console.log(W_im);
+
+  // determine size of P and initialize
+  // note P is the sum of squares 1 + 2^2 + 4^2 + ... + (n/2)^2
+  const wlen = Math.ceil(W.length/2);
+  const plen = wlen*(wlen+1)*(2*wlen+1)/6;
+  const P = new Float32Array(plen);//[];
+  //P.length = plen;
+  // prefill to avoid one step
+  //P.fill(1);
+  // iterator for P
+  let k = -1;
+  for (let i=0; i<=m-1; i++) {
+    // iterate from lower, right corner up and to the left
+    // this is to produce P in a sequence that
+    // can be simply iterated over in the matching step
+    // else Pm_11 ... Pm_nn | ... | P2_11 P2_21 P2_12 P2_22 | P1_11
+    // instead of more easily Pm_11 ... Pm_nn | ... | P2_22 P2_12 P2_21 P2_11 | P1_11
+
+    //console.time(`iter ${n-2*i-2}`)
+
+    //let positions = [];
+    //console.log(`i=${i}`);
+    // (let y=m-i; y--;)  can replace (let y=m-i-1; y>=0; y -= 1)
+    const shift = (n+1)*i;
+    for (let x=m-i; x--;) {
+      for (let y=m-i; y--;) {
+        // increase iterator
+        k++;
+        const pos = 2*(x*n + y) + shift;
+        //positions.push(pos);
+
+        const I1 = W_im[pos];
+        const I3 = W_im[pos+1];
+        const I2 = W_im[pos+n];
+        const I4 = W_im[pos+n+1];
+
+        const b = I1 + I4;
+        const b23 = I2 + I3;
+
+        if (b23 > b) {
+          W_im[pos] *= -1;
+          W_im[pos+1] = I2 - b;
+          W_im[pos+n] = I3 - b;
+          W_im[pos+n+1] *= -1;
+
+          const temp = W_real[pos+1];
+          //const D = W_real[pos] * W_real[pos+n+1];
+          const r1 = 1/W_real[pos];
+          const r2 = 1/W_real[pos+n+1];
+          W_real[pos+1] = W_real[pos+n]*r1*r2; ///D;
+          W_real[pos+n] = temp*r1*r2; ///D;
+          W_real[pos] = r1;//1/W_real[pos];
+          W_real[pos+n+1] = r2;//1/W_real[pos+n+1];
+
+          P[k] = 1;
+        } else if (b == b23) {
+          W_im[pos] *= -1;
+          W_im[pos+1] *= -1;
+          W_im[pos+n] *= -1;
+          W_im[pos+n+1] *= -1;
+
+          const R1 = W_real[pos];
+          const R3 = W_real[pos+1];
+          const R2 = W_real[pos+n];
+          const R4 = W_real[pos+n+1];
+
+          let D = R1 * R4 + R2 * R3;
+
+          W_real[pos] = R4 / D;
+          W_real[pos+1] = R2 / D;
+          W_real[pos+n] = R3 / D;
+          W_real[pos+n+1] = R1 / D;
+
+          P[k] = (R1 * R4) / D;
+        } else {
+          W_im[pos] = I4 - b23;
+          W_im[pos+1] *= -1;
+          W_im[pos+n] *= -1;
+          W_im[pos+n+1] = I1 - b23;
+
+          const temp = W_real[pos];
+          //const D = W_real[pos+1] * W_real[pos+n];
+          const r3 = 1 / W_real[pos+1];
+          const r2 = 1 / W_real[pos+n];
+          W_real[pos] = W_real[pos+n+1] * r2 * r3;// D;
+          W_real[pos+n+1] = temp * r2 * r3; // D;
+          W_real[pos+1] = r3;
+          W_real[pos+n] = r2;
+        }
+      }
+    }
+    //console.log(positions);
+    // console.log(W_real);
+    // console.log(W_im);
+    //
+    // console.log(`izero: ${izero}`);
+    // console.timeEnd(`iter ${n-2*i-2}`);
+  }
+  //console.log(P);
+  return P;
+}
+
+function Matching(P,n) {
+  // create empty (n+2)x(n+2) matrix M. We pad by 1 to simplify algorithm
+  // at corners. The padding is then stripped away at the end
+  const M = new Uint8Array((n+2)*(n+2));
+  const m = n/2;
+  let k = 0;
+  let ns = n+2;
+  const plen = P.length;
+
+  // let creationtime = 0;
+  // let deletiontime = 0;
+
+  for (let i = 0; i<=m; i++) {
+    // store 'active' cells that need to be filled in the creation step
+    // const Mcells = [];
+    // let bndFirst = m-i+1;
+    // let bndLast = m+i-1;
+    // iterate over cells to match, from top-left,
+    // columnwise down and then one step right
+    const shift = (1+ns)*(m-i+1);
+
+    // which cells are for creation Mc
+    const Mc = [];
+    // let d0 = performance.now();
+    for (let x=0; x<=2*(i-1); x += 2) {
+      for (let y=0; y<=2*(i-1); y += 2) {
+            //k++;
+            const pos = x * ns + y + shift;
+
+            // if (M[pos] && M[pos + ns + 1]) {
+            //   M[pos] = 0;
+            //   M[pos + ns +1] = 0;
+            //   Mc.push(x,y);
+            // } else if (M[pos + ns] && M[pos+1]) {
+            //   M[pos + ns] = 0;
+            //   M[pos+1] = 0;
+            //   Mc.push(x,y);
+            // } else if (M[pos]) {
+            //   M[pos] = 0;
+            //   M[pos + ns +1] = 1;
+            // } else if (M[pos + ns +1]) {
+            //   M[pos] = 1;
+            //   M[pos + ns +1] = 0;
+            // } else if (M[pos + ns]) {
+            //   M[pos + ns] = 0;
+            //   M[pos+1] = 1;
+            // } else if (M[pos+1]) {
+            //   M[pos + ns] = 1;
+            //   M[pos+1] = 0;
+            // } else {
+            //   Mc.push(x,y);
+            // }
+            switch (M[pos] + M[pos + 1] + M[pos + ns]  + M[pos + ns + 1]) {
+                case 1: // slide if only 1 edge
+                    const temp = M[pos];
+                    M[pos] = M[pos + ns +1];
+                    M[pos + ns +1] = temp;
+                    const temp2 = M[pos + ns];
+                    M[pos + ns] = M[pos+1];
+                    M[pos+1] = temp2;
+                    break;
+                case 2: // destroy if 2 edges
+                    M[pos] = 0;
+                    M[pos + 1] = 0;
+                    M[pos + ns] = 0;
+                    M[pos + ns + 1] = 0;
+                    //if (M[pos - ns]) { }
+                    // store active cell
+                    Mc.push(x,y);
+                    break;
+                case 0:
+                    // store active cell
+                    Mc.push(x,y);
+                    break;
+              }
+        }
+    }
+
+    // deletiontime += performance.now() - d0;
+    // let c0 = performance.now();
+    // the creation step, fill using Mcells
+    for (let r=0, mlen = Mc.length; r<mlen; r+= 2) {
+        // get location
+        //const pos = Mc[r];
+        const x = Mc[r];
+        const y = Mc[r+1];
+        const pos = x * ns + y + shift;
+        //if (i < 4) { console.log([ktest, Mc[r+1]])}
+        // check if cells below/to the left have been filled
+        // note: creation step depends on order of filling
+        //if (!(M[pos - 1] || M[pos - ns] ||  M[pos + 2*ns] || M[pos + ns - 1])) {
+        if (!(M[pos - 1] || M[pos - ns] ||  M[pos + 2] || M[pos + ns - 1])) {
+            // randomly assign new matching according to weight calculated in P
+            //if (Math.random() < P[plen-Mc[r+1]]) {
+            const k = plen - (y/2*i + x/2 + 1 + (i-1)*i*(2*i-1)/6);
+            //if (Math.random() < P[plen-k]) {
+            if (Math.random() < P[k]) {
+                M[pos] = 1;
+                M[pos + ns + 1] = 1;
+            } else {
+                M[pos + 1] = 1;
+                M[pos + ns] = 1;
+            }
+        }
+    }
+
+
+    // creationtime += performance.now() - c0;
+  }
+  // console.log(deletiontime);
+  // console.log(creationtime);
+  // strip away the padding and invert in y-axis
+  // one could also avoid this and re-define drawHexagon to skip padding
+  // or just return the matching in the corner.
+  const Mnew = [];
+  for (let i=n; i>=1; i--) {
+    const Mrow = new Uint8Array(n);
+    for (let j=1; j<=n; j++) {
+      Mrow[j-1] = M[i * ns + j];
+    }
+    Mnew.push(Mrow);
+  }
+  return Mnew;
+}
+
+function Coloring(P,n) {
+  const C = new Float32Array((n+2)*(n+2));
+  const m = n/2;
+  let ns = n+2;
+  let plen = P.length;
+
+  for (let i = 0; i<=m; i++) {
+    // iterate over cells to match, from top-left,
+    // columnwise down and then one step right
+    const shift = (1+ns)*(m-i+1);
+    for (let x=0; x<=2*(i-1); x += 2) {
+      for (let y=0; y<=2*(i-1); y += 2) {
+            const pos = x * ns + y + shift;
+            const p = C[pos];
+            const q = C[pos + 1];
+            const r = C[pos + ns];
+            const s = C[pos + ns + 1];
+
+            const deficit = 1-p-q-r-s;
+            // note bias = wz/(wz+xy)
+            const k = plen - (y/2*i + x/2 + 1 + (i-1)*i*(2*i-1)/6);
+            const bias = P[k];
+
+            C[pos]        = s + deficit*bias;
+            C[pos + 1]    = r + deficit*(1-bias);
+            C[pos + ns]    = q + deficit*(1-bias);
+            C[pos + ns +1] = p + deficit*bias;
+
+        }
+    }
+  }
+
+  const Mnew = [];
+  for (let i=n; i>=1; i--) {
+    const Mrow = new Float64Array(n);
+    for (let j=1; j<=n; j++) {
+      Mrow[j-1] = C[i * ns + j];
+    }
+    Mnew.push(Mrow);
+  }
+  return Mnew;
+}
+
+
+export {Reduction, Matching, Coloring};
--- weight_hexagon.js
+++ weight_hexagon.js
@@ -1,1 +1,444 @@
-export function weight_hexagon(e,t,o,r,n){const f=t*o,l=t*r,s=t*n,i=2*(f+l+s-1);for(var h=[],a=0;a<i;a++)h[a]=[],h[a].length=i,h[a].fill(0);const c=e.length,u=e[0].length;for(var g=0;g<i;g++)for(var v=0;v<i;v++)if(m(g,v))if(g%2==0){const t=g/2;h[i-1-v][g]=e[c-1-v%c][t%u]}else v%2==0&&(h[i-1-v][g]=1);else x=v,g>=2*(l+s)-1&&x>=2*(f+s)-1&&p(g,v)?h[i-1-v][g]=1:p(g,v)&&(v+g)%2==1&&(h[i-1-v][g]=1);var x;return h;function p(e,t){var o=!1;const r=i-1-t;return 0==e&&0==t?o=!0:0==e&&t>0?o=0==h[r+1][e]:e>0&&0==t?o=(e+t)%2==1?0==h[r][e-1]:0==h[r][e-1]&&0==h[r-1][e-1]:e>0&&t==i-1?o=(e+t)%2==1?0==h[r][e-1]&&0==h[r+1][e-1]&&0==h[r+1][e]:0==h[r][e-1]&&0==h[r+1][e]:e>0&&t>0&&t<i-1&&(o=(e+t)%2==1?0==h[r][e-1]&&0==h[r+1][e-1]&&0==h[r+1][e]:0==h[r][e-1]&&0==h[r-1][e-1]&&0==h[r+1][e]),o}function m(e,t){var o=!1;return e<2*Math.min(l,s)?o=t>=0&&t<2*f+e:e>=2*s&&e<2*l?o=t>=0&&t<2*(f+s)-1:e>=2*l&&e<2*s?o=t>=e-2*l+1&&t<2*f+e:e>=2*Math.max(l,s)&&e<2*(l+s)-1&&(o=t>=e-2*l+1&&t<2*(f+s)-1),o}}export function gap(e,t,o,r,n,f=0,l=0,s=1){const i=e.length;if(n>0){const h=o+r-1+2*f,a=i-(t+r)-2*l-f;if(1==s)for(let t=-n;t<=n;t++)for(let o=-2*n;o<=2*n;o++)o>=-n+t&&o<=n+t&&(e[a-t][h+o]=0,e.holes[a-t][h+o]=1);else if(2==s)for(let t=-n;t<=n;t++)for(let o=-n;o<=n;o++)e[a-t][h+o]=0,e.holes[a-t][h+o]=1;else if(3==s)for(let t=-n;t<=n;t++)for(let o=-2*n;o<=2*n;o++)o>=-n+t&&o<=n+t&&(e[a-o][h+t]=0,e.holes[a-o][h+t]=1);else if(4==s)for(let t=-1;t<=1;t++){for(let o=-1;o<=1;o++)e[a-o][h+t]=0,e.holes[a-o][h+t]=1;e[a][h]=1}}return e}
+export function weight_hexagon(w,A,B,C) {
+
+// make zero-filled array W
+const N = 2*(A+B+C-1);
+var W = [];
+for (var i=0; i<N; i++) {
+  W[i] = [];
+  W[i].length = N;
+  W[i].fill(0);
+}
+
+// const W = [];
+// for (let i=0; i<N; i++) {
+//   W[i] = new Float32Array(N);
+//   //W_im[i].length = n;
+//   //W_im[i].fill(0);
+// }
+
+
+// get dimensions
+const Ly = w.length;
+const Lx = w[0].length;
+
+// populate W
+for (var x = 0; x <N; x++) {
+  for (var y = 0; y<N; y++) {
+      //inside the hexagon
+      if (isinhexagon(x,y)) {
+        // if x is even,
+        if (x % 2 == 0) {
+          // check if correct
+          const k = x/2;
+          W[N-1-y][x] = w[Ly - 1 - (y % Ly)][k % Lx];
+        } else {
+          if (y % 2 == 0) {
+              W[N-1-y][x] = 1;
+          }
+        }
+      } else if (isinUpRightCorner(x,y) && isThereNoNeighbour(x,y)) {
+        W[N-1-y][x] = 1;
+      } else { // outside the hexagon
+        if (isThereNoNeighbour(x,y) && ((y+x) % 2 == 1)) {
+            W[N-1-y][x] = 1;
+        }
+      }
+  }
+}
+return W
+
+// find neighbour
+function isThereNoNeighbour(x, y) {
+  //N = W.length;
+  var boolean = false;
+  const x_ind = N-1-y;
+
+  if (x == 0 && y == 0) {
+    boolean = true;
+  } else if (x == 0 && y > 0) {
+    if (W[x_ind+1][x] == 0) {
+        boolean = true;
+    } else {
+        boolean = false;
+    }
+  } else if (x > 0 && y == 0) {
+    if ((x+y) % 2 == 1) {
+      if (W[x_ind][x-1] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    } else {
+      if (W[x_ind][x-1] == 0 && W[x_ind-1][x-1] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    }
+  } else if (x > 0 && y == N-1) {
+    if ((x+y) % 2 == 1) {
+      if (W[x_ind][x-1] == 0 && W[x_ind+1][x-1] == 0 && W[x_ind+1][x] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    } else {
+      if (W[x_ind][x-1] == 0 && W[x_ind+1][x] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    }
+  } else if (x > 0 && y > 0 && y < N-1) {
+    if ((x+y) % 2 == 1) {
+      if (W[x_ind][x-1] == 0 && W[x_ind+1][x-1] == 0 && W[x_ind+1][x] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    } else {
+      if (W[x_ind][x-1] == 0 && W[x_ind-1][x-1] == 0 && W[x_ind+1][x] == 0) {
+          boolean = true;
+      } else {
+          boolean = false;
+      }
+    }
+  }
+  return boolean;
+}
+
+// isinhexagon(x2,y2) return true if (x2,y2) is inside the hexagon.
+// return false otherwise
+function isinhexagon(x2,y2) {
+    var boolean = false;
+
+    if (x2 < 2*Math.min(B,C)) {
+        boolean = (y2 >= 0 && y2 < 2*A+x2);
+    } else if (x2 >= 2*C && x2 < 2*B) {
+        boolean = (y2 >= 0 && y2 < 2*(A+C)-1);
+    } else if (x2 >= 2*B && x2 < 2*C) {
+        boolean = (y2 >= x2-2*B+1 && y2 < 2*A+x2);
+    } else if (x2 >= 2*Math.max(B,C) && x2 < 2*(B+C)-1) {
+        boolean = (y2 >= x2-2*B+1 && y2 < 2*(A+C)-1);
+    }
+
+    return boolean;
+}
+
+// isinUpRightCorner(x3,y3) return true if (x3,y3) is in the top right
+// corner outside the hexagon.
+// return false otherwise
+function isinUpRightCorner(x3,y3) {
+    return (x3 >= 2*(B+C)-1 && y3 >= 2*(A+C)-1);
+}
+}
+
+// export function gap2(W,A,B,C,m,x=0,y=0,type=1,n=0) {
+//   const height = W.length;
+//
+//   if (m > 0) {
+//   const centerx = (B+C)-1 + 2*x;
+//   const centery = height - (A+C) - 2*y - x;
+//
+//     // if (type == 4) {
+//     //   for (let y = 0; y <= m; y++) {
+//     //    for (let x = -2*m; x <= 2*m; x++) {
+//     //     if (x >= y && x <= m+y) {
+//     //       // if (y % 2 == 0) {
+//     //       //   // remove weights from hole
+//     //       //   W[centery - y][centerx + x] = 0;
+//     //       // } else {
+//     //       //   // remove weights from hole
+//     //       //   W[centery - y][centerx + x] = 0;
+//     //       // }
+//     //       // remove weights from hole
+//     //       W[centery - y][centerx + x] = 0;
+//     //       // add hole to W.holes
+//     //       W.holes[centery - y][centerx + x] = 1;
+//     //       }
+//     //     }
+//     //   }
+//     if (type == 1) {
+//       for (let y = -m; y <= m; y++) {
+//        for (let x = -2*m; x <= 2*m; x++) {
+//         if (x >= -m+y && x <= m+y) {
+//           // if (y % 2 == 0) {
+//           //   // remove weights from hole
+//           //   W[centery - y][centerx + x] = 0;
+//           // } else {
+//           //   // remove weights from hole
+//           //   W[centery - y][centerx + x] = 0;
+//           // }
+//           // remove weights from hole
+//           W[centery - y][centerx + x] = 0;
+//           // add hole to W.holes
+//           W.holes[centery - y][centerx + x] = 1;
+//           }
+//         }
+//       }
+//     } else if (type == 2) {
+//       for (let y = -m; y <= m; y++) {
+//        for (let x = -m; x <= m; x++) {
+//           W[centery - y][centerx + x] = 0;
+//           // add hole to W.holes
+//           W.holes[centery - y][centerx + x] = 1;
+//         }
+//       }
+//     } else if (type == 3) {
+//       for (let x = -m; x <= m; x++) {
+//        for (let y = -2*m; y <= 2*m; y++) {
+//         if (y >= -m+x && y <= m+x) {
+//           W[centery - y][centerx + x] = 0;
+//           // add hole to W.holes
+//           W.holes[centery - y][centerx + x] = 1;
+//           }
+//         }
+//       }
+//     }
+//   } if (type == 4) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-1-2*y;
+//         const xpos = centerx+1+2*x;
+//         W[ypos][xpos] = 1;
+//         W[ypos+1][xpos] = 0;
+//         W[ypos-1][xpos] = 0;
+//         W[ypos][xpos+1] = 0;
+//         W[ypos][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   } else if (type == 5) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-1-2*y;
+//         const xpos = centerx+2*(x+y)+2;
+//         W[ypos][xpos] = 1;
+//         W[ypos][xpos-1] = 0;
+//         W[ypos][xpos+1] = 0;
+//         W[ypos+1][xpos+1] = 0;
+//         W[ypos-1][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   } if (type == 6) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-2*(y+x)-2;
+//         const xpos = centerx+1+2*x;
+//         W[ypos][xpos] = 1;
+//         W[ypos-1][xpos] = 0;
+//         W[ypos-1][xpos+1] = 0;
+//         W[ypos+1][xpos] = 0;
+//         W[ypos+1][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   }
+//
+//   //W.holes = holes;
+//   return W
+//
+// }
+
+// export function gap(W,A,B,C,m,xc=0,yc=0,type=1,n) {
+//   const height = W.length;
+//   if (m > 0) {
+//   //const centerx = (B+C)-1 + 2*x;
+//   const centerx = (B+C)-1 + xc + yc;
+//   //const centery = height - (A+C) - 2*y - x;
+//   const centery = height - (A+C) - yc;
+//   if (type == 1) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-1-2*y;
+//         const xpos = centerx+1+2*x;
+//         W[ypos][xpos] = 1;
+//         W[ypos+1][xpos] = 0;
+//         W[ypos-1][xpos] = 0;
+//         W[ypos][xpos+1] = 0;
+//         W[ypos][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   } else if (type == 2) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-1-2*y;
+//         const xpos = centerx+2*(x+y)+2;
+//         W[ypos][xpos] = 1;
+//         W[ypos][xpos-1] = 0;
+//         W[ypos][xpos+1] = 0;
+//         W[ypos+1][xpos+1] = 0;
+//         W[ypos-1][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   } else if (type == 3) {
+//       for (let y = 0; y < n; y++) {
+//       for (let x = 0; x < m; x++) {
+//         const ypos = centery-2*(y+x)-2;
+//         const xpos = centerx+1+2*x;
+//         W[ypos][xpos] = 1;
+//         W[ypos-1][xpos] = 0;
+//         W[ypos-1][xpos+1] = 0;
+//         W[ypos+1][xpos] = 0;
+//         W[ypos+1][xpos-1] = 0;
+//         // add hole to W.holes
+//         W.holes[ypos][xpos] = 1;
+//       }
+//       }
+//   } else if (type == 4 && A*B*C>=8) {
+//         const ypos = centery;
+//         const xpos = centerx;
+//
+//         W[ypos+1][xpos-2] = 0;
+//
+//         W[ypos-1][xpos-1] = 0;
+//         W[ypos][xpos-1] = 1;
+//         W[ypos+1][xpos-1] = 0;
+//         W[ypos+2][xpos-1] = 0;
+//         W.holes[ypos][xpos-1] = 1;
+//
+//         for (let x = 0; x < m; x++) {
+//           const xpos = centerx+2*x;
+//
+//           W[ypos-1][xpos] = 0;
+//           W[ypos+1][xpos] = 1;
+//
+//           W[ypos-2][xpos+1] = 0;
+//           W[ypos-1][xpos+1] = 1;
+//           W[ypos][xpos+1] = 0;
+//           W[ypos+1][xpos+1] = 0;
+//
+//           // add hole to W.holes
+//           W.holes[ypos+1][xpos] = 1;
+//           W.holes[ypos-1][xpos+1] = 1;
+//         }
+//
+//
+//         W[ypos-1][xpos+2] = 0;
+//     }
+//
+//   }
+//   return W
+// }
+
+
+export function gap(W,A,B,C,m,x=0,y=0,type=1,n) {
+  const height = W.length;
+  if (m > 0) {
+  const centerx = (B+C)-1 + 2*x;
+  const centery = height - (A+C) - 2*y - x;
+  if (type == 1) {
+      for (let y = 0; y < n; y++) {
+      for (let x = 0; x < m; x++) {
+        const ypos = centery-1-2*y;
+        const xpos = centerx+1+2*x;
+        W[ypos][xpos] = 1;
+        W[ypos+1][xpos] = 0;
+        W[ypos-1][xpos] = 0;
+        W[ypos][xpos+1] = 0;
+        W[ypos][xpos-1] = 0;
+        // add hole to W.holes
+        W.holes[ypos][xpos] = 1;
+      }
+      }
+  } else if (type == 2) {
+      for (let y = 0; y < n; y++) {
+      for (let x = 0; x < m; x++) {
+        const ypos = centery-1-2*y;
+        const xpos = centerx+2*(x+y)+2;
+        W[ypos][xpos] = 1;
+        W[ypos][xpos-1] = 0;
+        W[ypos][xpos+1] = 0;
+        W[ypos+1][xpos+1] = 0;
+        W[ypos-1][xpos-1] = 0;
+        // add hole to W.holes
+        W.holes[ypos][xpos] = 1;
+      }
+      }
+  } else if (type == 3) {
+      for (let y = 0; y < n; y++) {
+      for (let x = 0; x < m; x++) {
+        const ypos = centery-2*(y+x)-2;
+        const xpos = centerx+1+2*x;
+        W[ypos][xpos] = 1;
+        W[ypos-1][xpos] = 0;
+        W[ypos-1][xpos+1] = 0;
+        W[ypos+1][xpos] = 0;
+        W[ypos+1][xpos-1] = 0;
+        // add hole to W.holes
+        W.holes[ypos][xpos] = 1;
+      }
+      }
+  } if (type == 4) {
+        for (let y = -2*m; y <= 2*m; y++) {
+         for (let x = -2*2*m; x <= 2*2*m; x++) {
+          if (x >= -2*m+y && x <= 2*m+y) {
+            // re2*move weights fro2*m hole
+            W[centery - y][centerx + x] = 0;
+            // add hole to W.holes
+            W.holes[centery - y][centerx + x] = 1;
+            }
+          }
+        }
+      } else if (type == 5) {
+        for (let y = -2*m; y <= 2*m; y++) {
+         for (let x = -2*m; x <= 2*m; x++) {
+            W[centery - y][centerx + x] = 0;
+            // add hole to W.holes
+            W.holes[centery - y][centerx + x] = 1;
+          }
+        }
+      } else if (type == 6) {
+        for (let x = -2*m; x <= 2*m; x++) {
+         for (let y = -2*2*m; y <= 2*2*m; y++) {
+          if (y >= -2*m+x && y <= 2*m+x) {
+            W[centery - y][centerx + x] = 0;
+            // add hole to W.holes
+            W.holes[centery - y][centerx + x] = 1;
+            }
+          }
+        }
+      }
+  // else if (type == 4 && A*B*C>=8) {
+  //     const ypos = centery;
+  //     const xpos = centerx;
+  //
+  //     W[ypos+1][xpos-2] = 0;
+  //
+  //     W[ypos-1][xpos-1] = 0;
+  //     W[ypos][xpos-1] = 1;
+  //     W[ypos+1][xpos-1] = 0;
+  //     W[ypos+2][xpos-1] = 0;
+  //     W.holes[ypos][xpos-1] = 1;
+  //
+  //     for (let x = 0; x < m; x++) {
+  //       const xpos = centerx+2*x;
+  //
+  //       W[ypos-1][xpos] = 0;
+  //       W[ypos+1][xpos] = 1;
+  //
+  //       W[ypos-2][xpos+1] = 0;
+  //       W[ypos-1][xpos+1] = 1;
+  //       W[ypos][xpos+1] = 0;
+  //       W[ypos+1][xpos+1] = 0;
+  //
+  //       // add hole to W.holes
+  //       W.holes[ypos+1][xpos] = 1;
+  //       W.holes[ypos-1][xpos+1] = 1;
+  //     }
+  //
+  //
+  //     W[ypos-1][xpos+2] = 0;
+  // }
+
+  }
+  return W
+}
